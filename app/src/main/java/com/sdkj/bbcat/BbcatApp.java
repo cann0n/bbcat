fb515c3c935ff493e206b2701d01c4e96d5331e6
Rhino <793797714@qq.com>
2015-12-29 19:07:55 +0800

    增加环信相关依赖

--- app/src/main/java/com/sdkj/bbcat/BbcatApp.java
+++ app/src/main/java/com/sdkj/bbcat/BbcatApp.java
@@ -1,9 +1,17 @@
 package com.sdkj.bbcat;
 
+import com.easemob.chat.EMChat;
 import com.huaxi100.networkapp.application.BaseApplication;
 
 /**
  * Created by ${Rhino} on 2015/12/16 18:23
  */
 public class BbcatApp extends BaseApplication{
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        EMChat.getInstance().init(this);
+
+        EMChat.getInstance().setDebugMode(true);
+    }
 }
